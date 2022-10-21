0fac4d8bfdb1dbbdbd9ed11d16fa893c423ee5ce
Wallace Sá Lima <wallacealmeida@gmail.com>
2022-10-21 14:30:10 -0300

    Subindo Tarefa

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="estilo_H1">Hello World React</h1>
+      <h1 className="estilo_h1">Hello World React</h1>
+      <h2 className="mudar_Cor">First Course Assignment</h2>
     </div>
   );
 }
